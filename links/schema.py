318e5f72798e19358eca539b292dbbe8137f680b
Jeff Cohen <jcohen28@gmail.com>
2018-12-17 15:32:12 -0500

    add voting

--- links/schema.py
+++ links/schema.py
@@ -1,19 +1,27 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 
-from .models import Link
+from .models import Link, Vote
 from users.schema import UserType
 
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
-    
+
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
+    votes = graphene.List(VoteType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+    
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
@@ -41,5 +49,29 @@ class CreateLink(graphene.Mutation):
             posted_by=link.posted_by,
         )
 
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+    
+    def mutate(self, info, link_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('You must be logged to vote!')
+        
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise GraphQLError('Invalid link!')
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+        )
+
+        return CreateVote(user=user, link=link)
+
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
